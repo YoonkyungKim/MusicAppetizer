393c4f5c162fd44a1b36192f622d437e279b87af
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2021-02-17 22:22:23 -0500

    Modify server.js

--- server.js
+++ server.js
@@ -112,8 +112,8 @@ app.get('/callback', function(req, res) {
         });
 
         // we can also pass the token to the browser to make requests from there
-        // *redirect to client side
-        res.redirect('/spotify_login' +
+        // *redirect to client side//
+        res.redirect('/#' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
