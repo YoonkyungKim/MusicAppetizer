320ac986cfb1d3fb74b7f855388a5b859760248d
YoonkyungKim <yoonkyungkim.nlp@gmail.com>
2021-02-17 22:01:20 -0500

    Modify server.js

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get('/callback', function(req, res) {
 
         // we can also pass the token to the browser to make requests from there
         // *redirect to client side
-        res.redirect('http://localhost:8888/#' +
+        res.redirect('/' +
           querystring.stringify({
             access_token: access_token,
             refresh_token: refresh_token
